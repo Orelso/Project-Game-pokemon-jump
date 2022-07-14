51ca1c2bcb3ae0604990057ae98d11d97f124bf1
Orelso <orelso25@gmail.com>
2022-07-14 12:14:40 +0300

    update

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
 
-function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
+const urlArray = [
+    'https://www.serebii.net/swordshield/pokemon/001.png',
+    'https://www.serebii.net/swordshield/pokemon/002.png',
+    'https://www.serebii.net/swordshield/pokemon/003.png',
+  ]
+  
+
+function jump() {
+    if(character.classList == "animate")
+    {return}character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
 }
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
-        alert("Game Over! Score: "+Math.floor(counter/100));
+        alert("Game Over! Score: " + Math.floor(counter/100));
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
@@ -22,3 +30,4 @@ var checkDead = setInterval(function() {
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
 }, 10);
+
